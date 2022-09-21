8b38c210ce17dc27f2ed627fcfb4d9f6f1adc8af
Sasha Nikitin <sasha.nikitin7009@gmail.com>
2022-09-21 11:47:40 +0300

    my template
    my template

---
+++ js/script.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel({
+    // items:1,
+    autoWidth:true,
+    loop:true,
+    // center:true,
+    margin:15,
+    // nav: true,
+    // navElement: 'img'
+    // responsiveClass:true,
+    // responsive:{
+    //     0:{
+    //         items:1,
+    //         nav:true
+    //     },
+    //     600:{
+    //         items:3,
+    //         nav:false
+    //     },
+    //     1000:{
+    //         items:5,
+    //         nav:true,
+    //         loop:false
+    //     }
+    // }
+  });
+});
+
